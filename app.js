60017dda9c65b71dc80b3a7044051bce6fc4429d
Ryan <petroff.ryan@gmail.com>
2015-07-28 22:08:28 -0400

    Clarity fix
    No one knows what a gigasecond is.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 ideaCatalyst = {};
 ideaCatalyst.subjects = ["Genetics","Philosophy","Politics","Electronics","Linguistics","Friendship","War","Peace","Education","Efficiency","Telecommunications","Mathematics","Space/Time","Existentialism","Chemistry","Biology","Computer Science","Internet","Cooking","Fishing","Trolling","Countertrolling","Terrorism","Doublethink","Financial Engineering"];
 ideaCatalyst.objects = ["Statue","Book","Manifesto","Consumer Product","Bacteria","Computer","Food","Building","Icon","Avatar","Clothing","Bike","Vehicle","Financial Product","Contract","Animal","Accessory","Tool","Sticker","Poster","Plan"];
-ideaCatalyst.eras = ["Tomorrow","Next Year","Next Decade","Next Century","Next Generation","Next Gigasecond","Next Millennium"];
+ideaCatalyst.eras = ["Tomorrow","Next Year","Next Decade","Next Century","Next Generation","Next Gigasecond (Thirty Years From Now)","Next Millennium"];
 ideaCatalyst.moods = ["Optimistic","Pessimistic","Suspicious","Loving","Tired","Inquisitive","Quixotic","Frantic","Hysterical","Enraged","Indignant","Joyous","Excited","Horny","Hella"];
 
 ideaCatalyst.idea = function () {
