d4821d8cae17542c363dcaad7ba77c88e0e1d6f2
Ryan <petroff.ryan@gmail.com>
2015-07-28 21:36:56 -0400

    Adding in the output function

--- app.js
+++ app.js
@@ -3,3 +3,14 @@ ideaCatalyst.subjects = [];
 ideaCatalyst.objects = [];
 ideaCatalyst.eras = [];
 ideaCatalyst.moods = [];
+
+ideaCatalyst.idea = function () {
+  newIdea = {};
+  newIdea.subject = ideaCatalyst.subjects[Math.floor(Math.random() * ideaCatalyst.subjects.length)];
+  newIdea.object = ideaCatalyst.objects[Math.floor(Math.random() * ideaCatalyst.objects.length)];
+  newIdea.era = ideaCatalyst.eras[Math.floor(Math.random() * ideaCatalyst.eras.length)];
+  newIdea.mood = ideaCatalyst.moods[Math.floor(Math.random() * ideaCatalyst.moods.length)];
+  return newIdea;
+};
+
+console.log(ideaCatalyst.idea());
